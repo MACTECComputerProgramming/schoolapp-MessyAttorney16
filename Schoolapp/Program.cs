6ad0832bbe71c687530d7b5af88ec586912091ad
MessyAttorney16 <ljhalstead16@gmail.com>
2021-02-03 11:05:33 -0600

    commiting again

--- Schoolapp/Program.cs
+++ Schoolapp/Program.cs
@@ -6,10 +6,61 @@ namespace Schoolapp
     {
         static void Main(string[] args)
         {
-              
+
+            //create first student
+
+            Student FirstStudent = new Student();
+
+            //get student info
+
+            FirstStudent.StudentNum = AskForStudentNumber();
+            FirstStudent.StudentFirstName = AskForStudentName("First");
+            FirstStudent.StudentLastName = AskForStudentName("Last");
+            FirstStudent.major = AskForMajor(FirstStudent.StudentFirstName);
+            FirstStudent.Score1 = AskForExamScore(1);
+            FirstStudent.Score2 = AskForExamScore(2);
+            FirstStudent.Score3 = AskForExamScore(3);
+
+            Console.Clear();
+            Console.WriteLine("First Student");
+            Console.WriteLine(FirstStudent.ToString());
+
+            Console.WriteLine("Press Any Key to End");
+            Console.ReadKey();
 
 
 
+        }
+
+        private static string AskForExamScore(int whichOne)
+        {
+            string inValue;
+            int aScore;
+        }
+
+        private static string AskForMajor(string studentFirstName)
+        {
+            string inValue;
+            Console.WriteLine("Enter {0}\'s Major: ");
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+
+        private static string AskForStudentName(string whichOne)
+        {
+            string inValue;
+            Console.WriteLine("Enter Students {0} Name: ", whichOne);
+            inValue = Console.ReadLine();
+            return inValue;
+
+        }
+
+        private static string AskForStudentNumber()
+        {
+            string inValue;
+            Console.WriteLine("Enter Student ID Number: ");
+            inValue = Console.ReadLine();
+            return inValue;
 
         }
     }
--- Schoolapp/Student.cs
+++ Schoolapp/Student.cs
@@ -53,13 +53,28 @@ namespace Schoolapp
         //properties
 
         public string StudentNum { get => studentNum; set => studentNum = value; }
+        public string StudentLastName { get => studentLastName; set => studentLastName = value; }
+        public string StudentFirstName { get => studentFirstName; set => studentFirstName = value; }
+        public string major { get => major; set => major = value; }
+        public string Score1 { get => Score1; set => Score1 = value; }
+        public string Score2 { get => Score2; set => Score2 = value; }
+        public string Score3 { get => Score3; set => Score3 = value; }
+
 
 
 
         //methods
 
+        public double CalculatedAverage()
+        {
+            return (score1 + score2 + score3) / 3.0;
 
+        }
 
+        public override string ToString()
+        {
+            return "Name: " + studentFirstName + " " + studentLastName + "\nMajor: " + "\nScore Average: " + CalculatedAverage().ToString("F2");
+        }
 
     }
 }
