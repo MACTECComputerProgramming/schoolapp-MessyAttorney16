7cd717d4f7dda005b9db060bb1b66fcad920170a
MessyAttorney16 <ljhalstead16@gmail.com>
2021-02-04 10:27:25 -0600

    final commit

--- Schoolapp/Program.cs
+++ Schoolapp/Program.cs
@@ -25,9 +25,35 @@ namespace Schoolapp
             Console.WriteLine("First Student");
             Console.WriteLine(FirstStudent.ToString());
 
-            Console.WriteLine("Press Any Key to End");
+            Console.WriteLine("Press enter for next student");
             Console.ReadKey();
 
+            //create second student
+
+            Console.WriteLine("\nSecond Student \n");
+
+            Student SecondStudent = new Student("6789", "Randall", "Frank");
+            SecondStudent.major = AskForMajor(SecondStudent.StudentFirstName);
+            SecondStudent.Score1 = AskForExamScore(1);
+            SecondStudent.Score2 = AskForExamScore(2);
+            SecondStudent.Score3 = AskForExamScore(3);
+
+            Console.WriteLine("Second Student");
+            Console.WriteLine(SecondStudent.ToString());
+
+            Console.WriteLine("Press enter for next student");
+            Console.ReadKey();
+
+            Console.WriteLine("\nGetting Data From Database\n");
+
+            //create third student
+            Student ThirdStudent = new Student("4567", "Breakers", "Jeff", "History", 98, 90, 95);
+
+            Console.WriteLine("Third Student");
+            Console.WriteLine(ThirdStudent.ToString());
+
+            Console.WriteLine("Press any key to end program");
+            Console.ReadKey();
 
 
         }
@@ -36,6 +62,12 @@ namespace Schoolapp
         {
             string inValue;
             int aScore;
+
+            Console.WriteLine("Enter a value for score {0}:", whichOne);
+            inValue = Console.ReadLine();
+            aScore = int.Parse(inValue);
+            return aScore;
+
         }
 
         private static string AskForMajor(string studentFirstName)
